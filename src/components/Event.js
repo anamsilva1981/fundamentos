5645dc4586183349275364c78cfb6d8c7962d70c
anamsilva1981 <ana.msilva@hotmail.com>
2026-01-16 04:16:59 -0300

    feat(eventos): adicionando funções dentro de evento onClick

--- src/components/Event.js
+++ src/components/Event.js
@@ -8,7 +8,16 @@ function Event() {
     }
   return (
     <div>
-        <button onClick={handleClick}>Clique aqui!</button>
+        <button onClick={handleClick}>Botão de evento</button> <br></br>
+
+        <button onClick={() => { console.log('função dentro do evento')}}>Botão com função dentro do evento</button><br></br>
+        
+        <button onClick={() => { 
+            if(true){console.log(true, 'função com validação dentro do evento')}
+        }}
+        >Botão com função com validação dentro do evento</button><br></br>
+        
+        
     </div>
   )
 }
